daaa1e4d6c0a98add2cea301d4f78b10509691c6
Francisco Alisson Vargas <falisson.sv@gmail.com>
2019-05-25 21:55:16 -0300

    Create user registration method

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,26 +1,72 @@
 const request = require('request');
+const bcrypt = require('bcryptjs');
+
+
+const User = require('../models/User');
+
 
 class Auth {
-    getToken() {
-        const credentials = Buffer.from("clientId:clientSecret")
-        .toString('base64');
+	getApplicationToken() {
+		const credentials = Buffer.from("clientId:clientSecret")
+			.toString('base64');
 
-        const options = {
-            url: 'https://accounts.spotify.com/api/token',
-            method: "POST",
-            headers: {
-                'Content-Type': "application/x-www-form-urlencoded",
-                'Authorization': `Basic ${credentials}`
-            },
-            form: {grant_type: 'client_credentials'}
-        }
+		const options = {
+			url: 'https://accounts.spotify.com/api/token',
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Authorization': `Basic ${credentials}`
+			},
+			form: { grant_type: 'client_credentials' }
+		}
 
-        request(options, function (error, response, body) {
+		request(options, function (error, response, body) {
 			if (error) throw new Error(error);
 
 			console.log(body);
 		});
-    }
+	}
+
+	async registerUser(req, res) {
+		try {
+			console.log(req.body);
+			await userNotExists(req.body.email);
+			req.body.password = await encrypt(req.body.password);
+			const user = await User.create(req.body);
+
+			return res.json(user);
+		} catch (err) {
+			return Promise.reject(err);
+		}
+	}
+
+
+}
+
+function encrypt(data) {
+	return new Promise((resolve, reject) => {
+		bcrypt.genSalt(5, (error, salt) => {
+			bcrypt.hash(data, salt, (err, hash) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(hash);
+				}
+			});
+		});
+	});
+}
+
+async function userNotExists(email) {
+	return new Promise((resolve, reject) => {
+		User.findOne({ email: email }, function (err, user) {
+			if (!user && err == null) {
+				resolve(true);
+			} else if (user) {
+				reject("Há um usuário cadastrado com esse endereço de email.");
+			}
+		});
+	});
 }
 
 module.exports = new Auth();
--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Search = require('./controllers/search')
+const mongoose = require('mongoose');
+
+const User = require('./models/User')
+const Search = require('./controllers/search');
+const Auth = require('./controllers/auth');
 const app = express();
 
 // Configurations from server
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+mongoose.connect(
+	'mongodb+srv://admin:yottaadmin@discoderiadb-qdlfx.mongodb.net/test?retryWrites=true',
+	{
+		useNewUrlParser: true
+	}
+);
+
 app.get('/search', Search.fetchResults);
 
+app.post('/auth/register', Auth.registerUser);
 
 app.listen(process.env.PORT || 3001, () => {
-    console.log('Servidor iniciado com sucesso!');
+	console.log('Servidor iniciado com sucesso!');
 });
 
